837c227ea320d6ea42f12f9bb899dc73f3489cd4
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-02 16:18:47 +0900

    Add: Titleの挿入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Title from "./components/TItle";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="Test">
+            <Title/>
+        </div>
+    );
 }
 
 export default App;
