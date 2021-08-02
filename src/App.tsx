7b8aa56047f34329d209682e78edea59878d86c9
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-02 16:27:33 +0900

    Add: Formの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Title from "./components/TItle";
+import Form from "./components/Form";
 
 function App() {
     return (
         <div className="Test">
             <Title/>
+            <Form />
         </div>
     );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+const Form = ()=>{
+    return(
+        <form action="">
+            <input type="text" name="city" placeholder="都市名"/>
+            <button type="submit">天気を取得</button>
+        </form>
+    );
+};
+
+export default Form;
