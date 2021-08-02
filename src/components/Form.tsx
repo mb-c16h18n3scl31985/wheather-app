ddd5938b8eb145ee323b6233d905fcd33ec7a161
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-02 16:45:07 +0900

    Add: stateのセット

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,11 @@
 import {useState} from "react";
 
-const Form = ()=>{
-    const [city,setCity]=useState("");
-    return(
+const Form = () => {
+    const [city, setCity] = useState<string>("");
+    return (
         <form action="">
-            <input type="text" name="city" placeholder="都市名"/>
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+            {city}
             <button type="submit">天気を取得</button>
         </form>
     );
