6ffbd121831ba9e9395f45940bd623b5379c6ca8
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-02 19:16:27 +0900

    Add: Resultsコンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Title from "./components/TItle";
 import Form from "./components/Form";
+import Results from "./components/Results";
 
 function App() {
     return (
         <div className="Test">
             <Title/>
-            <Form />
+            <Form/>
+            <Results/>
         </div>
     );
 }
