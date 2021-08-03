d727bed93f69571dde2669480f4d6e5517c17774
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-08-03 16:13:06 +0900

    Add: formの分割代入

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,10 +3,10 @@ type FormPropsType = {
     getWeather: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 const
-    Form: React.FC<FormPropsType> = (props) => {
+    Form: React.FC<FormPropsType> = ({setCity,getWeather}: FormPropsType) => {
         return (
-            <form onSubmit={props.getWeather}>
-                <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
+            <form onSubmit={getWeather}>
+                <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
                 <button type="submit">天気を取得</button>
             </form>
         );
